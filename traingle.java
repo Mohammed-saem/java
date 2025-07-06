3ddf9953f24601ac1faa2fda5242a1060f1efd13
Mohammed-saem <saembehlim@gmail.com>
2025-07-06 15:11:33 +0530

    Add files via upload

---
+++ traingle.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class conditon {
+    public static void main(String[]args){
+      Scanner sc= new Scanner (System.in);
+
+ // whenever we can print the perttern then using nested loop
+    int n =4;
+    int m=6;
+        for (int i=1;i<=n;i++){ // using for total lines of code u can see in this code have 4 row lines
+  for (int j=1;j<=m;j++){ // using line changes like m euqal to 6 then print 6 time *
+      System.out.print(" *" );
+  }
+      System.out.println();
+  }
+
+      }
+    }
